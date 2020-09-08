d7214b6fab765b424a041706e5c8f003935b49e8
WSM <wmarcuse@gmail.com>
2020-09-08 22:02:27 +0200

    Renamed services directory

---
+++ aws/services/docker-run-frontend-service-locally.sh
@@ -0,0 +1,7 @@
+winpty docker run -it --rm \
+-p 8080:8080 \
+-e AWS_ACCESS_KEY=$AWS_ACCESS_KEY \
+-e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY \
+-e AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION \
+-e AWS_APP_CONTEXT=$AWS_APP_CONTEXT \
+575950781373.dkr.ecr.eu-central-1.amazonaws.com/focus/frontend:$1
